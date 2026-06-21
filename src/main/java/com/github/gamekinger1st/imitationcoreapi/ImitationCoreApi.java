4d7e6e5a7ea6c5ad966cffb53ef5f2ecef9eebda
Gamekinger1st <shoemakeasher0@Gmail.com>
2026-06-21 00:08:03 -0400

    Remove nonessential comments

--- src/main/java/com/github/gamekinger1st/imitationcoreapi/ImitationCoreApi.java
+++ src/main/java/com/github/gamekinger1st/imitationcoreapi/ImitationCoreApi.java
@@ -2,7 +2,6 @@ package com.github.gamekinger1st.imitationcoreapi;
 
 import net.neoforged.fml.common.Mod;
 
-/** Entry point for shared imitation infrastructure. */
 @Mod(ImitationCoreApi.MOD_ID)
 public final class ImitationCoreApi {
     public static final String MOD_ID = "imitationcoreapi";
